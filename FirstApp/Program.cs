22616263848a0c3e375cddf47a72f944399b6175
Bogdan Tanasuca <manuel_708@yahoo.com>
2018-10-06 22:32:43 +0300

    Add project files.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstApp
+{
+    enum TagType
+    {
+        HTML=1,
+        BODY=2,
+        HEAD=3,
+        TITLE=4,
+        DIV=5,
+        H1=6
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.File.WriteAllText("index.html", "");
+            Tag html = new Tag(TagType.HTML);
+            Tag body = new Tag(TagType.BODY);
+            Tag head = new Tag(TagType.HEAD);
+            Tag title = new Tag(TagType.TITLE);
+            Tag div = new Tag(TagType.DIV);
+            Tag h1 = new Tag(TagType.H1);
+
+            html.AddChild(head);
+            html.AddChild(body);
+            head.AddChild(title);
+            title.SetContent("Titlu");
+            body.AddChild(div);
+            div.AddChild(h1);
+            div.AddAttribute("color", "blue");
+            h1.SetContent("Gigi 4 the WIN!!");
+
+            html.Render();
+        }
+    }
+
+
+}
---
+++ FirstApp/Tag.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstApp
+{
+    class Tag
+    {
+        private TagType Type;
+        private IDictionary<string, string> Attributes = new Dictionary<string, string>();
+        private string Content;
+        private int Depth;
+        List<Tag> Children = new List<Tag>();
+        private Tag() { }
+        public Tag(TagType Type)
+        {
+            this.Type = Type;
+            this.Content = "";
+            this.Depth = 0;
+        }
+        public void AddChild(Tag child)
+        {
+            this.Children.Add(child);
+            child.Depth = this.Depth + 1;
+            Console.WriteLine(child.Depth + " " + child.Type);
+        }
+        public void AddAttribute(string attribute,string value)
+        {
+            this.Attributes.Add(attribute, value);
+        }
+        public void SetContent(string content)
+        {
+            this.Content = content;
+        }
+        public void print()
+        {
+            foreach (KeyValuePair<string, string> kvp in this.Attributes)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}",kvp.Key, kvp.Value);
+            }
+        }
+        public void Render()
+        {
+
+            string TagLine = "<" + this.Type;
+            foreach (KeyValuePair<string, string> kvp in this.Attributes)
+            {
+                TagLine += " " + kvp.Key + "=" + kvp.Value;
+            }
+            TagLine += ">";
+
+            System.IO.File.AppendAllText("index.html", TagLine + Environment.NewLine);
+            if (this.Content != "")
+            {
+                for (int i = 0; i < this.Depth+1; i++)
+                {
+                    System.IO.File.AppendAllText("index.html", "\t");
+                }
+                System.IO.File.AppendAllText("index.html", this.Content + Environment.NewLine);
+            }
+            
+            foreach (var child in this.Children)
+            {
+                for(int i=0;i<=this.Depth;i++)
+                {
+                    System.IO.File.AppendAllText("index.html", "\t");
+                }
+                child.Render();
+                System.IO.File.AppendAllText("index.html",Environment.NewLine);
+            }
+            for (int i = 0; i < this.Depth; i++)
+            {
+                System.IO.File.AppendAllText("index.html", "\t");
+            }
+            TagLine = "</" + this.Type + ">";
+            System.IO.File.AppendAllText("index.html", TagLine);
+        }
+    }
+}
